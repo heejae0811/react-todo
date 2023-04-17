9ace26a213631501c216668a11b0716d0b09e8a2
이희재 <hj.lee2@stickint.kr>
2023-04-17 15:34:26 +0900

    #1.4 Recap Refactor

--- src/index.js
+++ src/index.js
@@ -7,15 +7,20 @@ const number = document.querySelector('span')
 
 number.innerText = 0
 
-// 2. reducer를 만든다. reducer는 function(함수)이다.
-// 3. action을 만든다.
+// string을 직접 사용하기 보다는 변수로 만들어서 사용하는 것이 오타 등의 에러를 발견할 수 있어 효율적이다.
+const ADD = 'ADD'
+const MINUS = 'MINUS'
+
+// 2. reducer(countModifier)를 만든다. reducer는 function(함수)이다. reducer의 return값이 state가 된다.
+// 3. action을 만든다. (if문보다 switch문이랑 더 자주 사용된다. 공식문서.)
 const countModifier = (count = 0, action) => {
-  if(action.type === 'ADD') {
-    return count + 1
-  } else if(action.type === 'MINUS') {
-    return count - 1
-  } else {
-    return count
+  switch (action.type) {
+    case ADD:
+      return count + 1
+    case MINUS:
+      return count -1
+    default:
+      return count
   }
 }
 
@@ -27,12 +32,12 @@ const onChange = () => {
 }
 countStore.subscribe(onChange)
 
-// 4. dispatch(객체)를 만든다.
+// 4. dispatch(object, 객체)를 만든다. dispatch를 통해서 action을 보낸다.
 const handleAdd = () => {
-  countStore.dispatch({type: 'ADD'})
+  countStore.dispatch({type: ADD})
 }
 const handleMinus = () => {
-  countStore.dispatch({type: 'MINUS'})
+  countStore.dispatch({type: MINUS})
 }
 
 add.addEventListener('click', handleAdd)
