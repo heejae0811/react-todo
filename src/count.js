706fe590ca3202cdec47db7699e2d008ef7bda5d
이희재 <hj.lee2@stickint.kr>
2023-04-27 16:34:15 +0900

    #1 PURE REDUX: COUNTER

--- src/count.js
+++ src/count.js
@@ -1,19 +1,20 @@
-// store는 나의 state(바뀌는 데이터)를 저장하는 곳이다.
 import { createStore } from 'redux'
 
-const add = document.getElementById('add')
+// html 태그 선택
+const plus = document.getElementById('plus')
 const minus = document.getElementById('minus')
-const number = document.querySelector('span')
+const number = document.getElementById('number')
 
+// 초기값
 number.innerText = 0
 
 // string을 직접 사용하기 보다는 변수로 만들어서 사용하는 것이 오타 등의 에러를 발견할 수 있어 효율적이다.
 const ADD = 'ADD'
 const MINUS = 'MINUS'
 
-// 2. reducer(countModifier)를 만든다. reducer는 function(함수)이다. reducer의 return값이 state가 된다.
-// 3. action을 만든다. (if문보다 switch문이랑 더 자주 사용된다. 공식문서.)
-const countModifier = (count = 0, action) => {
+// 2. reducer(변수 이름은 자유)를 만든다.
+const reduceCountModifier = (count = 0, action) => {
+  // 3. action을 만든다.
   switch (action.type) {
     case ADD:
       return count + 1
@@ -25,20 +26,24 @@ const countModifier = (count = 0, action) => {
 }
 
 // 1. store를 만든다.
-const countStore = createStore(countModifier)
+const countStore = createStore(reduceCountModifier)
 
+// store 안에 있는 state를 가지고 온다.
 const onChange = () => {
   number.innerText = countStore.getState()
 }
+
+// store 안에 있는 state의 변화를 감지한다.
 countStore.subscribe(onChange)
 
-// 4. dispatch(object, 객체)를 만든다. dispatch를 통해서 action을 보낸다.
-const handleAdd = () => {
+// 4. dispatch를 만든다.
+const handlePlus = () => {
   countStore.dispatch({type: ADD})
 }
 const handleMinus = () => {
   countStore.dispatch({type: MINUS})
 }
 
-add.addEventListener('click', handleAdd)
+// 클릭 이벤트
+plus.addEventListener('click', handlePlus)
 minus.addEventListener('click', handleMinus)
